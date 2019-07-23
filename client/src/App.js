2e62d016177f6ce5252597e78c3afb6fa494e492
aryaminus <sunim.54@gmail.com>
2019-07-23 16:13:01 +0545

    Add Login button to hit the /auth/facebook endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// custom components
+import FacebookLogin from "react-facebook-login";
 
 function App() {
+  const responseFacebook = response => {
+    console.log(response);
+    const tokenBlob = new Blob(
+      [JSON.stringify({ access_token: response.accessToken }, null, 2)],
+      { type: "application/json" }
+    );
+    const options = {
+      method: "POST",
+      body: tokenBlob,
+      mode: "cors",
+      cache: "default"
+    };
+    fetch("/auth/facebook", options).then(r => {
+      const token = r.headers.get("x-auth-token");
+      r.json().then(user => {
+        if (token) {
+          console.log(user, token);
+        }
+      });
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookLogin
+        appId="2430182417265110"
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
     </div>
   );
 }
