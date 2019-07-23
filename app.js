f51f450ab2a45301d11f3767ef5399ab9df5d979
aryaminus <sunim.54@gmail.com>
2019-07-23 17:17:51 +0545

    Setup Passport to get the token and then generate custom JWT token

--- app.js
+++ app.js
@@ -1,17 +1,75 @@
 const express = require("express"),
   passport = require("passport"),
-  FacebookStrategy = require("passport-facebook").Strategy,
-  session = require("express-session"),
+  FacebookTokenStrategy = require("passport-facebook-token"),
+  cors = require("cors"),
   cookieParser = require("cookie-parser"),
-  bodyParser = require("body-parser"),
-  config = require("./configuration/config"),
-  mysql = require("mysql"),
-  app = express();
+  bodyParser = require("body-parser");
 
+const config = require("./configuration/config"),
+  { generateToken, sendToken } = require("./configuration/token");
+
+const app = express(),
+  corsOption = {
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    exposedHeaders: ["x-auth-token"]
+  };
+
+// Use the FacebookStrategy within Passport.
+passport.use(
+  new FacebookTokenStrategy(
+    {
+      clientID: config.facebook_api_key,
+      clientSecret: config.facebook_api_secret
+    },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
+        return done(null, profile);
+      });
+    }
+  )
+);
+
+app.use(cors(corsOption));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: "keyboard cat", key: "sid" }));
-app.use(passport.initialize());
-app.use(passport.session());
+
+app.get("/", function(req, res) {
+  res.render("index", { user: req.user });
+});
+
+app.post(
+  "/auth/facebook",
+  passport.authenticate("facebook-token", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      return res.send(401, "User Not Authenticated");
+    }
+    req.auth = {
+      id: req.user.id
+    };
+    next();
+  },
+  generateToken,
+  sendToken
+);
+
+// app.get("/account", ensureAuthenticated, function(req, res) {
+//   res.render("account", { user: req.user });
+// });
+
+// app.get("/logout", function(req, res) {
+//   req.logout();
+//   res.redirect("/");
+// });
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return next();
+//   }
+//   res.redirect("/login");
+// }
 
 app.listen(4000);
---
+++ configuration/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  facebook_api_key: "2430182417265110",
+  facebook_api_secret: "70c6aa87a216b2f8e7ec75d94f314213",
+  // callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: true,
+  host: "localhost",
+  username: "root",
+  password: "5432",
+  database: "arms"
+};
