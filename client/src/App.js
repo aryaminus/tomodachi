3b8d78ac73dd79a051c041364949c056ffd056af
aryaminus <sunim.54@gmail.com>
2019-07-24 19:54:17 +0545

    Add styled-components with Gradient Background SVG

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+
+//styles
+import * as S from "./styles";
+import background from "./images/background.svg";
 
 // custom components
 import FacebookLogin from "react-facebook-login";
 
 function App() {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
+  const [token, setToken] = useState("");
+
   const responseFacebook = response => {
     console.log(response);
     const tokenBlob = new Blob(
@@ -22,18 +28,26 @@ function App() {
       const token = r.headers.get("x-auth-token");
       r.json().then(user => {
         if (token) {
-          console.log(user, token);
+          setAuthenticated(true);
+          setUser(user);
+          setToken(token);
         }
       });
     });
   };
+
   return (
     <div className="App">
-      <FacebookLogin
-        appId="2430182417265110"
-        fields="name,email,picture"
-        callback={responseFacebook}
-      />
+      <S.Background src={background} />
+      {!isAuthenticated ? (
+        <S.FacebookContainer>
+          <FacebookLogin
+            appId="2430182417265110"
+            fields="name,email,picture"
+            callback={responseFacebook}
+          />
+        </S.FacebookContainer>
+      ) : null}
     </div>
   );
 }
---
+++ client/src/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FacebookContainer = styled.div({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  left: "50%",
+  top: "50%",
+  transform: "translate(-50%, -50%)"
+});
+
+export const Background = styled.img({
+  position: "fixed",
+  left: 0,
+  top: 0,
+  right: 0,
+  bottom: 0,
+  width: "100vw",
+  maxWidth: "100vw",
+  height: "100vh",
+  overflow: "hidden",
+  objectPosition: "top",
+  objectFit: "cover",
+  transition: "all 150ms ease-in",
+  opacity: 1
+});
