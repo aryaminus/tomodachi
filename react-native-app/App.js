8d00487ce8bcbc323d35b933bd1b325ed7e5d7af
aryaminus <sunim.54@gmail.com>
2019-07-27 17:36:28 +0545

    Setup react-native-app

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -6,29 +6,21 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  StatusBar
+} from "react-native";
 
 const App = () => {
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      {/* <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
@@ -67,48 +59,48 @@ const App = () => {
             <LearnMoreLinks />
           </View>
         </ScrollView>
-      </SafeAreaView>
+      </SafeAreaView> */}
     </Fragment>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter
+//   },
+//   engine: {
+//     position: "absolute",
+//     right: 0
+//   },
+//   body: {
+//     backgroundColor: Colors.white
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: "600",
+//     color: Colors.black
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: "400",
+//     color: Colors.dark
+//   },
+//   highlight: {
+//     fontWeight: "700"
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: "600",
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: "right"
+//   }
+// });
 
 export default App;
